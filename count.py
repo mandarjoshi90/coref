045733b0f172f1bb53bd7c0ca9557582400aa605
Mandar Joshi <mandarj@devfair0268.h2.fair>
2018-12-13 11:16:35 -0800

    wip

--- count.py
+++ count.py
@@ -35,16 +35,30 @@ def avg_len(data_file):
     total = 0
     max_num_sp = 0
     segments = []
+    limit = 250
+    limit_exceeding = 0
+    max_sent_len = 0
+    sent_lens = []
     for i, line in enumerate(f):
         # print('---', line)
         data = json.loads(line)
         text = util.flatten(data['sentences'])
         segments.append(len(data['sentences']))
         total += len(text)
+        long_sent = False
+        for sentence in data['sentences']:
+          max_sent_len = max(max_sent_len, len(sentence))
+          sent_lens.append(len(sentence))
+          if len(sentence) > limit:
+            long_sent = True
+            print(sentence, data['doc_key'])
+        limit_exceeding += int(long_sent)
         max_num_sp = max(max_num_sp, len(text))
     print(total / i)
     print(max_num_sp)
     print(len(segments), sum(segments) / len(segments), max(segments), sum([1 for s in segments if s == 1]))
+    print('limit_exceeding/max', limit_exceeding, max_sent_len)
+    print('topk', sorted(sent_lens, reverse=True)[:20])
 
 def cluster_distance(data_file):
     f = open(data_file)
@@ -62,5 +76,5 @@ def cluster_distance(data_file):
 
 if __name__ == '__main__':
     data_file = sys.argv[1]
-    cluster_distance(data_file)
+    avg_len(data_file)
 
