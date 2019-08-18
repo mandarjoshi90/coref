78f0f0c52b512a22acd446e5b354950b57c5a507
Mandar Joshi <mandar90@cs.washington.edu>
2019-08-17 22:09:00 -0700

    modify setup script

--- setup_training.sh
+++ setup_training.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+
+ontonotes_path=$1
+data_dir=$2
+
 dlx() {
-  wget $1/$2
-  tar -xvzf $2
-  rm $2
+  wget -P $data_dir $1/$2
+  tar -xvzf $data_dir/$2 -C $data_dir
+  rm $data_dir/$2
 }
 
 conll_url=http://conll.cemantix.org/2012/download
@@ -13,16 +17,18 @@ dlx $conll_url/test conll-2012-test-key.tar.gz
 dlx $conll_url/test conll-2012-test-official.v9.tar.gz
 
 dlx $conll_url conll-2012-scripts.v3.tar.gz
-
 dlx http://conll.cemantix.org/download reference-coreference-scorers.v8.01.tar.gz
-mv reference-coreference-scorers conll-2012/scorer
 
-ontonotes_path=data/coref
-bash conll-2012/v3/scripts/skeleton2conll.sh -D $ontonotes_path/data/files/data conll-2012
+
+wget -P $data_dir https://storage.googleapis.com/bert_models/2018_10_18/cased_L-24_H-1024_A-16.zip
+unzip $data_dir/cased_L-24_H-1024_A-16.zip
+rm $data_dir/cased_L-24_H-1024_A-16.zip
+mv cased_L-24_H-1024_A-16 $data_dir/
+bash conll-2012/v3/scripts/skeleton2conll.sh -D $ontonotes_path/data/files/data $data_dir/conll-2012
 
 function compile_partition() {
     rm -f $2.$5.$3$4
-    cat conll-2012/$3/data/$1/data/$5/annotations/*/*/*/*.$3$4 >> $2.$5.$3$4
+    cat $data_dir/conll-2012/$3/data/$1/data/$5/annotations/*/*/*/*.$3$4 >> $data_dir/$2.$5.$3$4
 }
 
 function compile_language() {
@@ -35,4 +41,5 @@ compile_language english
 compile_language chinese
 compile_language arabic
 
-python minimize.py
+vocab_file=$data_dir/cased_L-24_H-1024_A-16/vocab.txt
+python minimize.py $vocab_file $data_dir $data_dir false
