0372016458e9121f340ba1872dd859309c1adae9
Mandar Joshi <mandar90@cs.washington.edu>
2019-10-27 22:51:23 -0700

    add spanbert orig download

--- setup_training.sh
+++ setup_training.sh
@@ -18,6 +18,15 @@ download_bert(){
   mv $model $data_dir/
 }
 
+download_spanbert(){
+  model=$1
+  wget -P $data_dir https://dl.fbaipublicfiles.com/fairseq/models/$model.tar.gz
+  mkdir $data_dir/$model
+  tar xvfz $data_dir/$model.tar.gz -C $data_dir/$model
+  rm $data_dir/$model.tar.gz
+}
+
+
 conll_url=http://conll.cemantix.org/2012/download
 dlx $conll_url conll-2012-train.v4.tar.gz
 dlx $conll_url conll-2012-development.v4.tar.gz
@@ -29,6 +38,8 @@ dlx http://conll.cemantix.org/download reference-coreference-scorers.v8.01.tar.g
 
 download_bert cased_L-12_H-768_A-12
 download_bert cased_L-24_H-1024_A-16
+download_spanbert spanbert_hf
+download_spanbert spanbert_hf_base
 
 bash conll-2012/v3/scripts/skeleton2conll.sh -D $ontonotes_path/data/files/data $data_dir/conll-2012
 
